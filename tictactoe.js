f64f72af930ec904c2acd14b96df123f9feb79b5
Diego <rssdgi@gmail.com>
2018-11-03 13:08:30 +0100

    Javascripts code with objects

--- tictactoe.js
+++ tictactoe.js
@@ -1,108 +1,119 @@
 var button=document.getElementById("button");
-var cellTable=document.getElementsByTagName("td");
-var whoStarts="";
-
-// clear the board
-function clearBoard(){
-	for (var i = cellTable.length - 1; i >= 0; i--) {
-		cellTable[i].textContent="";
-	};
-}
-
-function addEventListener(addEv=true){
-	for (var i = cellTable.length - 1; i >= 0; i--) {
-		// assign event onClick to all table cells objects
-		if (addEv){
-			cellTable[i].addEventListener("click", tictactoeFunction);
-		} else {
-			cellTable[i].removeEventListener("click", tictactoeFunction);
-		}
-	}
-}
-
-// setUp the board
-function showBoard(){
-	for (var i = cellTable.length - 1; i >= 0; i--) {
-		cellTable[i].style.border="5px solid black";
-		cellTable[i].style.color="black";
-	}
-	addEventListener(true);
-}
 
 button.onclick=function(event){
-	// get who starts if X or O
-	// default is X set in html radio button
-	showBoard();
-	var radio=document.getElementById("defaultChecked");
-	if (radio.checked){
-		whoStarts="X";
-	} else {
-		whoStarts="O";
-	}
-	console.log("Starts "+whoStarts);
-	clearBoard();
+	// start program
+	ticBoard.initCells();
+	ticBoard.clearBoard();
+	ticBoard.showBoard();
+	// add click event on cell and send to bameObj.showSymbolEv
+	// init gameClass
+	gameObj.setWhoStarts();
+	ticBoard.addRemoveCellsEvents();
+};
+
+var gameObj= {
+	// this class should use the boardCells obj and manage the game
+	// show the symbol O or X
+	showSymbolEv(event){
+		//console.log("Pressed on cell "+event.target.id+" whoStarts set to "+gameObj.whoStarts);
+		if (event.target.textContent==""){
+			event.target.textContent=gameObj.whoStarts;
+			// disable cell to avoid double X or O
+			ticBoard.removeCellEvent(event.target.id);
+		}
+		if (gameObj.whoStarts=="X"){
+			gameObj.whoStarts="O";
+		} else {
+			gameObj.whoStarts="X";
+		}
+		gameObj.checkGame();
+	},
+	// check from the radio buttons who starts if O or X
+	setWhoStarts(){
+		var radio=document.getElementById("defaultChecked");
+		if (radio.checked){
+			this.whoStarts="X";
+		} else {
+			this.whoStarts="O";
+		}
+		//console.log("Variable Whostarts set to "+this.whoStarts)
+	},
+	checkGame(){ 
+		// check if someone wins
+		var goodCombos=[[0,3,6],[0,1,2],[0,4,8],[1,4,7],[3,4,5],[2,4,6],[2,5,8],[6,7,8]];
+		var countX=0;
+		var countO=0;
+		for (var i = goodCombos.length - 1; i >= 0; i--) {
+			//cellTable[i] is cell obj
+			// check if any combo is on the table
+			for (var j = 0; j <= 2; j++) {
+				if (ticBoard.getCell(goodCombos[i][j]).textContent=="X"){
+					countX++;
+				}
+				if (ticBoard.getCell(goodCombos[i][j]).textContent=="O"){
+					countO++;
+				}
+			}
+			// if some combination is found 
+			if (countX==3){
+				// X WINS!!
+				//console.log("XX WINS FOR COMBO "+goodCombos[i]);
+				ticBoard.evidenceWinner(goodCombos[i]);
+				break;
+			}
+			if (countO==3){
+				// X WINS!!
+				//console.log("OO WINS FOR COMBO "+goodCombos[i]);
+				ticBoard.evidenceWinner(goodCombos[i]);
+				break;
+			}
+			countO=0;
+			countX=0;
+		}
+	},
 }
 
-// event function attached to cells
-function tictactoeFunction(event){
-	 //console.log(event.target.id+event.type);
-	if (event.target.textContent==""){
-		event.target.textContent=whoStarts;
-		// disable cell to avoid double X or O
-		event.target.removeEventListener("click", tictactoeFunction);
-	}
-	
-	if (whoStarts=="X"){
-		whoStarts="O";
-	} else {
-		whoStarts="X";
-	}
-	checkGame();	
-}	
-
-// winner found 
-function evidenceWinner(combo){
-	// combo is a 3 elements array that corrisponds
-	// to a good combo
-	for (var i = combo.length - 1; i >= 0; i--) {
-		cellTable[combo[i]].style.color="red";
-	}
-	// disable listener
-	addEventListener(false);
-}
-
-// check if there is a winner
-function checkGame(){
-	// check if someone wins
-	var goodCombos=[[0,3,6],[0,1,2],[0,4,8],[1,4,7],[3,4,5],[2,4,6],[2,5,8],[6,7,8]];
-	var countX=0;
-	var countO=0;
-
-	for (var i = goodCombos.length - 1; i >= 0; i--) {
-		//cellTable[i] is cell obj
-		// check if any combo is on the table
-		for (var j = 0; j <= 2; j++) {
-			if (cellTable[goodCombos[i][j]].textContent=="X"){
-				countX++;
-			}
-			if (cellTable[goodCombos[i][j]].textContent=="O"){
-				countO++;
+var ticBoard = {
+	initCells(){
+		// initialize cells as array of cells obj [0..8]
+		this.cells=document.getElementsByTagName("td")
+	},
+	//	return cell obj i
+	getCell(i){
+		return this.cells[i]
+	},
+	// clear the board content
+	clearBoard(){
+		for (var i = this.cells.length - 1; i >= 0; i--) {
+			this.cells[i].textContent="";
+		}
+	},
+	// show the board with dark border
+	showBoard(){
+		for (var i = this.cells.length - 1; i >= 0; i--) {
+			this.cells[i].style.border="5px solid black";
+			// set cell content color
+			this.cells[i].style.color="black";
+		}
+	},
+	addRemoveCellsEvents(addEv=true){
+		for (var i = this.cells.length - 1; i >= 0; i--) {
+		// assign event onClick to all table cells objects
+			if (addEv){
+				this.getCell(i).addEventListener("click", gameObj.showSymbolEv);
+			} else {
+				this.removeCellEvent(i);
 			}
 		}
-		// if some combination is found 
-		if (countX==3){
-			// X WINS!!
-			console.log("XX WINS FOR COMBO "+goodCombos[i]);
-			evidenceWinner(goodCombos[i]);
-			break;
+	},
+	removeCellEvent(i){
+		this.getCell(i).removeEventListener("click", gameObj.showSymbolEv);
+	},
+	evidenceWinner(combo){
+		for (var i = combo.length - 1; i >= 0; i--) {
+			this.getCell(combo[i]).style.color="red";
 		}
-		if (countO==3){
-			// X WINS!!
-			console.log("OO WINS FOR COMBO "+goodCombos[i]);
-			evidenceWinner(goodCombos[i]);
-			break;
-		}
-		countO=0;
-		countX=0;
+		// disable listener
+		this.addRemoveCellsEvents(false);
 	}
 }
